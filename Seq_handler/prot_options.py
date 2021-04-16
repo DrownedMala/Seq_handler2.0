a6b13fcf95d7732f0beffe19f40d250cd43b48b2
Mala <malago97@gmail.com>
2021-04-16 14:41:02 +0200

    s

--- Seq_handler/prot_options.py
+++ Seq_handler/prot_options.py
@@ -1,9 +1,11 @@
 from Bio.Seq import Seq
 from Bio.Blast import NCBIWWW
 
-
+new_seq = "abc"
 
 def prot_options(new_seq):
     # maybe a blast?
-    print("I'm working")
-    prot_seq = Seq(new_seq.upper())
+    print(f"I'm {new_seq}")
+    #prot_seq = Seq(new_seq.upper())
+
+prot_options(new_seq)
