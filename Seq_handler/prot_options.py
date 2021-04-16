7eff6db4e6411d24a84fe744977e915f86891ce4
Mala <malago97@gmail.com>
2021-04-16 16:13:00 +0200

    completa blast

--- Seq_handler/prot_options.py
+++ Seq_handler/prot_options.py
@@ -1,9 +1,22 @@
 from Bio.Seq import Seq
-from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIWWW, NCBIXML
 
 
 def prot_options(new_seq):
     # maybe a blast?
     prot_seq = Seq(new_seq.upper())
 
-print("ciao")
+    blasted = NCBIWWW.qblast("blastp", "nr", prot_seq)
+    blast_records = NCBIXML.parse(blasted)
+
+    threshold = 1e-50
+    for record in blast_records:
+        if record.alignments:
+            print(f"\nQuery: {record.query[:100]}\n")
+            for align in record.alignments:
+                for hsp in align.hsps:
+                    if hsp.expect < threshold:
+                        print(">")
+                        print(f"match: {align.title[:100]}")
+                        print(f"score: {hsp.score}")
+                        print(">\n")
