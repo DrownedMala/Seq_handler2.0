ce8002bf054e17c773c39abdd349f4857d481eac
Mala <malago97@gmail.com>
2021-04-16 15:36:02 +0200

    clearing some lines

--- Seq_handler/prot_options.py
+++ Seq_handler/prot_options.py
@@ -1,12 +1,8 @@
 from Bio.Seq import Seq
 from Bio.Blast import NCBIWWW
 
-new_seq = "abc"
 
 def prot_options(new_seq):
     # maybe a blast?
-    print(f"I'm {new_seq}")
-    #prot_seq = Seq(new_seq.upper())
+    prot_seq = Seq(new_seq.upper())
 
-prot_options(new_seq)
-print("Ciao uaglio'")
