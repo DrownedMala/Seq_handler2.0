ae0d34855b43fbbe568572a2c486c533b81a9bf9
Mala <malago97@gmail.com>
2021-04-20 11:47:43 +0200

    spacing

--- Seq_handler/fasta_options.py
+++ Seq_handler/fasta_options.py
@@ -5,6 +5,7 @@ import os, os.path
 from Bio import SeqIO
 
 
+
 #def Parse_and_write(path):
 # controlla l'esistenza del file in input, da splittare in seguito ↓
 isFile = False 
@@ -13,17 +14,22 @@ isFile = os.path.isfile(path) # se non è un file, entra il ciclo per richiedere
 while isFile == False: # controlla che il file esista, prima di creare una cartella per lasciarla vuota
     path = input("There is an error in the FASTA file's path, try again: ") # dovrebbe prelevare il file dal percorso datogli
     isFile = os.path.isfile(path)
- 
+
+
+
 # crea cartella di destinazione per i file divisi nella cwd ↓
 wd = f"{os.getcwd()}/{os.path.basename(os.path.normpath(path))}_Split" # assegna il nome del file originale anche alla cartella
 os.makedirs(f"{wd}")                 
 os.chdir(wd) # porta alla directory in cui finiranno i nuovi file                              
 
 
+
 # per ogni file diviso assegna: nome del file (descrizione org.), id e sequenza all'interno del file stesso ↓ 
 for record in SeqIO.parse(path , "fasta"): 
     with open(f"{str(record.description)[33:]}.fasta", "w") as f: 
        f.write(f"{str(record.id)}\n{str(record.seq)}") 
+
+
 
 # check di sicurezza: se trova o meno la cartella appena creata, avverte ↓
 isdir = os.path.isdir(wd)  
