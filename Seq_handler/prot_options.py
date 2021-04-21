ee3423aab3de970a4f8a013365af63387368c625
Mala <malago97@gmail.com>
2021-04-21 12:00:23 +0200

    gives choices

--- Seq_handler/prot_options.py
+++ Seq_handler/prot_options.py
@@ -5,21 +5,40 @@ from Bio.Blast import NCBIWWW, NCBIXML
 # restituisce titolo del risultato e score
 
 def aa_options(new_str):        
-    # maybe a blast?
-    prot_seq = Seq(new_str.upper())
     
-    
-    blasted = NCBIWWW.qblast("blastp", "nr", prot_seq)
-    blast_records = NCBIXML.parse(blasted)
+    what_to_do = input("""What are you willing to do now?\n
+                       You can:\n
+                       1) run a blast on the sequence you entered.\n
+                       2) --- \n
+                       >>> """)
+
+    if what_to_do == "1" or what_to_do.lower() == "blast":
+        print("A blast is being run by now, wait for the results!")
+        prot_seq = Seq(new_str.upper())
+        
+        
+        blasted = NCBIWWW.qblast("blastp", "nr", prot_seq)
+        blast_records = NCBIXML.parse(blasted)
 
 
-    threshold = 1e-40
-    for record in blast_records:
-        if record.alignments:
-            print(f"\nQuery: {record.query[:100]}\n")
-            for align in record.alignments:
-                for hsp in align.hsps:
-                    if hsp.expect < threshold:
-                        print(f"match: {align.title[:100]}")
-                        print(f"score: {hsp.score}")
-                        print(">\n")
+        threshold = 1e-40
+        for record in blast_records:
+            if record.alignments:
+                print(f"\nQuery: {record.query[:100]}\n")
+                for align in record.alignments:
+                    for hsp in align.hsps:
+                        if hsp.expect < threshold:
+                            print(f"match: {align.title[:100]}")
+                            print(f"E-value: {hsp.expect}")
+                            print("")
+
+
+# MKWVTFISLLFLFSSAYSRGVFRRDAHKSEVAHRFKDLGEENFKALVLIAFAQYLQQCPFEDHVKLVNEV
+# TEFAKTCVADESAENCDKSLHTLFGDKLCTVATLRETYGEMADCCAKQEPERNECFLQHKDDNPNLPRLV
+# RPEVDVMCTAFHDNEETFLKKYLYEIARRHPYFYAPELLFFAKRYKAAFTECCQAADKAACLLPKLDELR
+# DEGKASSAKQRLKCASLQKFGERAFKAWAVARLSQRFPKAEFAEVSKLVTDLTKVHTECCHGDLLECADD
+# RADLAKYICENQDSISSKLKECCEKPLLEKSHCIAEVENDEMPADLPSLAADFVESKDVCKNYAEAKDVF
+# LGMFLYEYARRHPDYSVVLLLRLAKTYETTLEKCCAAADPHECYAKVFDEFKPLVEEPQNLIKQNCELFE
+# QLGEYKFQNALLVRYTKKVPQVSTPTLVEVSRNLGKVGSKCCKHPEAKRMPCAEDYLSVVLNQLCVLHEK
+# TPVSDRVTKCCTESLVNRRPCFSALEVDETYVPKEFNAETFTFHADICTLSEKERQIKKQTALVELVKHK
+# PKATKEQLKAVMDDFAAFVEKCCKADDKETCFAEEGKKLVAASQAALGL
