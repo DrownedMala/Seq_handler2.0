ce387345f625feae76348ac5a8a12bf523f47106
Mala <malago97@gmail.com>
2021-04-22 12:40:46 +0200

    comments in english

--- Seq_handler/files_options.py
+++ Seq_handler/files_options.py
@@ -2,25 +2,25 @@
 import os, os.path
 from Bio import SeqIO
 
-# Programma che prende un file fasta/gbk con più record
-# e lo divide in un record per ogni file
+# takes a multi-record fasta/gbk file
+# and splits it into multiple files (1 seq/file)
 
 
 def fasta_splitter(new_str):
 
-    # crea cartella di destinazione per i file divisi nella cwd ↓
+    # creates destination folder for split files in the cwd ↓
     wd = f"{os.getcwd()}/Split_{os.path.basename(os.path.normpath(new_str))}" # assegna il nome del file originale anche alla cartella
     os.makedirs(f"{wd}")                 
-    os.chdir(wd) # porta alla directory in cui finiranno i nuovi file                              
+    os.chdir(wd) # takes you to the directory in which files will be placed                            
 
 
-    # per ogni file diviso assegna: nome del file (descrizione org.), id e sequenza all'interno del file stesso ↓ 
+    # for each new file: name of the file (descript. of org.); id e seq inside the file ↓ 
     for record in SeqIO.parse(new_str , "fasta"): 
         with open(f"{str(record.description)[33:]}.fasta", "w") as f: 
             f.write(f"{str(record.id)}\n{str(record.seq)}") 
 
 
-    # check di sicurezza: se trova o meno la cartella appena creata, avverte ↓
+    # security check: whether it finds or not the new folder, the script warns you ↓
     isdir = os.path.isdir(wd)  
     if isdir == True:
         print(f"\nA new folder \"Split_*\" was created as \"{os.getcwd()}\"")
@@ -50,6 +50,6 @@ def gbk_splitter(new_str):
         print("Something went wrong, folder was not created.")
 
 
-
+# reference paths of mine to test
 # /home/mala/Coding/Python/DataAnalysis/Pandas/ls_orchid.fasta    
 # /home/mala/Coding/Python/DataAnalysis/Pandas/ls_orchid.gbk
