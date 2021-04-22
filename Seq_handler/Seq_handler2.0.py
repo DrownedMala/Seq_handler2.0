3395f21f414c39be139161877a55715a9be5ed16
Mala <malago97@gmail.com>
2021-04-22 12:31:59 +0200

    comments in english

--- Seq_handler/Seq_handler2.0.py
+++ Seq_handler/Seq_handler2.0.py
@@ -2,40 +2,38 @@
 # http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec24
 
 from files_options import fasta_splitter, gbk_splitter
-from seqs_options import dna_options, rna_options  # contengono trascrizione, traduzione, trascrizione inversa, gc content
-from prot_options import aa_options                # contiene blast per ora
+from seqs_options import dna_options, rna_options  # contains gc content, transcription, translation and back-transcription
+from prot_options import aa_options                # contains blast for now
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
 import os.path
 
 
-# tolti gli aminoacidi che corrispondono a 'ACTG', queste sono le lettere che servono al programma 
-# per decidere se la sequenza sia amminoacidica o nucleotidica
+# aa letters, minus 'ATCG': this tuple allows the code to decide if a sequence is aminoacidic or not
 aa_only_letter = ("L", "M", "F", "W", "K", "Q", "E", "S", "P", "V", "I", "Y", "H", "R", "N", "D")
 
 
 
-# input iniziale
+# starting input 
 new_str = input("Insert your sequence or the path to a file >>> ")
 
 
 
-# se non ci sono slash probabilmente non è un percorso per un file.
+# if there are no slashes it's not a path.
 if "\\" not in new_str and "/" not in new_str:
-    # se abbiamo una sequenza abbastanza lunga dovrebbe essere possibile dedurne il tipo dalla sua composizione di 
-    # lettere, senza considerare la possibilità che siano solo ATCG e siano amminoacidi
+    # if we have a seq long enough, it should be possible to infere its type by its letters composition 
     # ↓
     if len(new_str) > 20:                               
         for i in new_str.upper():
             if i in aa_only_letter:
-                aa_options(new_str)                  # definire una funzione per le opzioni aminoacidi
+                aa_options(new_str)                  
                 break
             elif i == "U": 
                 rna_options(new_str)
                 break
         else:
-            dna_options(new_str)                      # definire una funzione per le opzioni dna 
-    # se la seq è corta, si chiede che tipo di sequenza sia
+            dna_options(new_str)                      
+    # if it's a short seq, it asks for its type
     # ↓
     else:   
         which_seq_type = input("Which type of seq did you input? \n[AA, RNA, DNA] >>> ")
@@ -47,7 +45,7 @@ if "\\" not in new_str and "/" not in new_str:
         elif which_seq_type.lower() == 'dna' or which_seq_type == '3':
             dna_options(new_str)
 
-#controlla che la stringa passata sia un percorso esistente per un file
+# check if the string is actually an existing path
 elif "\\" in new_str or "/" in new_str:
     while os.path.exists(new_str) == False: # controlla il percorso
         new_str = input("It seems you tried to work on a file, but the path isn't correct. Check it and retry. \n>>> ")
@@ -60,7 +58,3 @@ elif "\\" in new_str or "/" in new_str:
     
     if what_now == "1" or what_now.lower() == "split":
         fasta_splitter(new_str)
-    
-
-
-
